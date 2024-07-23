d02c1f5e7b1cd4a4a194c438fcf4e1f8e0f924ba
eduardoamaral-dev <eduardo.lopes.amaral@outlook.com>
2024-07-23 19:41:33 -0300

    ✨ creating project

---
+++ wp_challenge-api/src/main/java/io/vizier/wp_challengeapi/WpChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package io.vizier.wp_challengeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpChallengeApiApplication.class, args);
+	}
+
+}
---
+++ wp_challenge-api/src/test/java/io/vizier/wp_challengeapi/WpChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vizier.wp_challengeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
